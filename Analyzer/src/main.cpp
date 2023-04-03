67bc5190646d1086ed77335c3e5443f772c7c684
pyvyx <pyvyx@web.de>
2023-04-03 18:25:39 +0200

    Moved all of the window creation code into the example plot function

--- Analyzer/src/main.cpp
+++ Analyzer/src/main.cpp
@@ -8,11 +8,16 @@
 #include "Log.h"
 
 
-void LinePlot(int , const Window&)
+void LinePlot()
 {
+    //ImGui::SetNextWindowSize(window.GetSize());
+    ImGui::SetNextWindowPos({ 0, 0 });
+    ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0, 0));
+    ImGui::Begin("Test", nullptr, IMGUI_WINDOW_FLAGS);
+
     // f(x)=x^2
-    static float x[100], y[100];
-    for (int i = -50; i < 50; ++i)
+    static float x[101], y[101];
+    for (int i = -50; i <= 50; ++i)
     {
         x[i+50] = (float)i;
         y[i+50] = (float)(i * i);
@@ -20,33 +25,23 @@ void LinePlot(int , const Window&)
 
     if (ImPlot::BeginPlot("Line Plots", {-1,-1}))
     {
-        ImPlot::SetupAxisLimits(ImAxis_Y1, 0, 30);
+        //ImPlot::SetupAxisLimits(ImAxis_Y1, 0, 30);
         ImPlot::SetupAxes("x", "y");
         ImPlot::PlotLine("f(x)", x, y, 100);
         ImPlot::EndPlot();
     }
+    ImGui::End();
+    ImGui::PopStyleVar();
 }
 
 
 int main()
 {
-    int c = 50;
     const Window window;
     while (window.IsOpen())
     {
         window.StartFrame();
-        ImGui::SetNextWindowSize(window.GetSize());
-        ImGui::SetNextWindowPos({ 0, 0});
-        ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0, 0));
-        ImGui::Begin("Test", nullptr, IMGUI_WINDOW_FLAGS);
-        LinePlot(c, window);
-        ImGui::End();
-        ImGui::PopStyleVar();
         window.EndFrame();
-        //std::this_thread::sleep_for(std::chrono::milliseconds(50));
-        if (c == 1000)
-            c = 0;
-        c += 50;
     }
     return 0;
 }
