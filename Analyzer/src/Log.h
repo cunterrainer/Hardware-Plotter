c74aebb0fdd5b1a2442894a173cc86683c7cdf41
pyvyx <pyvyx@web.de>
2023-04-08 18:13:48 +0200

    Error messages will now be red in the console

--- Analyzer/src/Log.h
+++ Analyzer/src/Log.h
@@ -3,6 +3,7 @@
 #include <ostream>
 #include <string>
 #include <system_error>
+#include <Windows.h>
 #define Endl std::endl
 #define GetWinError() Logger::Error(GetLastError())
 
@@ -14,29 +15,37 @@ public:
         return "Error: " + std::to_string(error) + " (" + std::system_category().message(static_cast<int>(error)) + ")";
     }
 private:
+    static constexpr WORD OutputColorWhite = 0b0111;
+    static constexpr WORD OutputColorLightRed = 0b1100;
+private:
     const char* const m_LogInfo;
-    mutable bool newLine = true;
+    mutable bool m_NewLine = true;
+    mutable bool m_IsErr = false;
+    mutable HANDLE m_Console = GetStdHandle(STD_OUTPUT_HANDLE);
 public:
-    inline explicit Logger(const char* info) noexcept : m_LogInfo(info) {}
+    inline explicit Logger(const char* info, bool isErr) noexcept : m_LogInfo(info), m_IsErr(isErr) {}
 
     inline const Logger& operator<<(std::ostream& (*osmanip)(std::ostream&)) const noexcept
     {
         std::cout << *osmanip;
-        newLine = true;
+        m_NewLine = true;
+        SetConsoleTextAttribute(m_Console, OutputColorWhite);
         return *this;
     }
 
     template <class T>
     inline const Logger& operator<<(const T& mess) const noexcept
     {
-        if (newLine)
+        if (m_NewLine)
         {
+            if (m_IsErr)
+                SetConsoleTextAttribute(m_Console, OutputColorLightRed);
             std::cout << m_LogInfo;
-            newLine = false;
+            m_NewLine = false;
         }
         std::cout << mess;
         return *this;
     }
 };
-inline const Logger Log("[INFO] ");
-inline const Logger Err("[ERROR] ");
+inline const Logger Log("[INFO] ", false);
+inline const Logger Err("[ERROR] ", true);
