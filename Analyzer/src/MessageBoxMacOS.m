865fdf4a7799c933ac339a8acaed2625d025b746
pyvyx <vyx3327@web.de>
2023-09-02 00:16:42 +0200

    changed the name to better represent the purpose

--- Analyzer/src/MessageBoxMacOS.m
+++ Analyzer/src/MessageBoxMacOS.m
@@ -6,7 +6,6 @@ void MessageBoxErrorMacOS(const char* message)
     NSString* msg = [[NSString alloc] initWithUTF8String:message];
     NSAlert *alert = [[NSAlert alloc] init];
     [alert addButtonWithTitle:@"OK"];
-    NSLog(@"NSString %@", msg);
     [alert setMessageText:@"Error"];
     [alert setInformativeText:msg];
     [alert setAlertStyle:NSAlertStyleWarning];
